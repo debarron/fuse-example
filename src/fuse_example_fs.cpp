060074b4d3ddfc4bbe71e986b14e91326f27863c
Daniel Lopez <de.lopezbarron@gmail.com>
2020-06-05 12:22:14 -0600

    Adding the extern C code

--- src/fuse_example_fs.cpp
+++ src/fuse_example_fs.cpp
@@ -8,21 +8,31 @@ extern "C" {
 #include <stdio.h>
 #include <string.h>
 
+void nf_fs_init(){cpp_fs_init();}
+
+void nf_fs_add(const char *entry){cpp_fs_add(entry);}
+
+char *nf_fs_find(const char *key){cpp_fs_find(key);}
+
+char **nf_fs_list(){cpp_fs_list();}
+}
+
+
 using namespace std;
 map<string, string> my_fs;
 map<string, string>::iterator it;
 
-void nf_fs_init(){
+void cpp_fs_init(){
   my_fs.clear();
   my_fs["/"] = "First entry";
 }
 
-void nf_fs_add(const char *entry){
+void cpp_fs_add(const char *entry){
   string fs_entry = string(entry);
   my_fs[fs_entry] = "New entry added";
 }
 
-char *nf_fs_find(const char *key){
+char *cpp_fs_find(const char *key){
   string fs_key = string(key);
   char *found = NULL;
 
@@ -33,7 +43,7 @@ char *nf_fs_find(const char *key){
   return found;
 }
 
-char **nf_fs_list(){
+char **cpp_fs_list(){
   char **entry_list; 
   entry_list = (char **)malloc(sizeof(char*) * my_fs.size());
 
@@ -46,4 +56,4 @@ char **nf_fs_list(){
   return entry_list;
 }
 
-}
+
--- src/fuse_example_fs.h
+++ src/fuse_example_fs.h
@@ -10,4 +10,13 @@ char *nf_fs_find(const char *key);
 
 char **nf_fs_list();
 }
+
+void cpp_fs_init();
+
+void cpp_fs_add(const char *entry);
+
+char *cpp_fs_find(const char *key);
+
+char **cpp_fs_list();
+
 #endif
