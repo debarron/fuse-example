672a82206cc0ad04ea4810d4328fe66fa054e398
Daniel Lopez <de.lopezbarron@gmail.com>
2020-06-15 23:59:31 -0600

    Adding the last changes

--- src/fuse_example_main.c
+++ src/fuse_example_main.c
@@ -167,10 +167,10 @@ static int nf_readdir(
     ? nf_fs_list_root(&n_entries) 
     : nf_fs_list_dir(dir, &n_entries);
   
-  filler(".", NULL, 0, 0);
-  filler("..", NULL, 0, 0);
+  filler(buffer,".", NULL, 0, 0);
+  filler(buffer,"..", NULL, 0, 0);
   for(int i = 0; i < n_entries; i++)
-    filler(entries[i], NULL, 0, 0);
+    filler(buffer,entries[i], NULL, 0, 0);
 
   // append all elements to filler
   // filler(.., NULL, 0)
